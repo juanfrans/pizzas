34ac836857f987ff60099dc5e5a6471b383d9cf5
Juan Francisco Saldarriaga <jfs2118@columbia.edu>
2021-06-17 14:50:11 -0400

    added data and setup basic popup

--- js/map.js
+++ js/map.js
@@ -1,5 +1,6 @@
 mapboxgl.accessToken = "pk.eyJ1IjoienNjaG5laWRlciIsImEiOiJjaXg3eWUzeGowMXEyMnlxeWI1MzBudzN0In0.i-aef9w2ifwlPvXerrQOwA";
 
+// Create and initialize map variable
 var map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/zschneider/ckpohayy112xe17qlj1asuw3i",
@@ -7,13 +8,13 @@ var map = new mapboxgl.Map({
   zoom: 11.25,
 });
 
+// Load other layers
 map.on('load', function() {
     var layers = map.getStyle().layers;
     var firstSymbolId;
     for (var i = 0; i < layers.length; i++) {
       if (layers[i].type === "symbol") {
         firstSymbolId = layers[i].id;
-        // console.log(firstSymbolId);
         break;
       }
     }
@@ -45,4 +46,23 @@ map.on('load', function() {
       },
       firstSymbolId
     );
-})
+});
+
+// Create popups
+map.on("click", "pizzaPlaces", function (e) {
+  e.originalEvent.preventDefault();
+  console.log("Clicked on a pizza place...");
+  let popuphtml =
+    "<div class='mt2 mb2 dark-gray'><h4 class='ttu mb0'>Pizza Place X</h4>" +
+    "<p class='mt2'>Placeholder text for address, url, telephone, and 3d model.</p>";
+  popup = new mapboxgl.Popup()
+    .setLngLat(e.lngLat)
+    .setHTML(popuphtml)
+    .addTo(map);
+});
+map.on("mouseenter", "pizzaPlaces", function () {
+  map.getCanvas().style.cursor = "pointer";
+});
+map.on("mouseleave", "pizzaPlaces", function () {
+  map.getCanvas().style.cursor = "";
+});
